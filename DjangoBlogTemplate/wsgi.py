1a63d2929c4b71a923562d16a091b445af7374bb
Dalekvim <dalekvim@gmail.com>
2020-08-15 19:18:15 +0100

    Initial commit

---
+++ DjangoBlogTemplate/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DjangoBlogTemplate project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBlogTemplate.settings')
+
+application = get_wsgi_application()
