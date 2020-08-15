458dea9ecec3c82917f5f834c9b45ddeef612e45
Dalekvim <dalekvim@gmail.com>
2020-08-15 21:25:59 +0100

    set up post and base.html

--- DjangoBlogTemplate/settings.py
+++ DjangoBlogTemplate/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import Model, CharField, ForeignKey, DateTimeField
+from django.contrib.auth.models import User
+from django.utils import timezone
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Post(Model):
+  title = CharField(max_length=150)
+  author = ForeignKey(User, on_delete=models.CASCADE)
+  date_posted = DateTimeField(default=timezone.now)
+  content = HTMLField()
